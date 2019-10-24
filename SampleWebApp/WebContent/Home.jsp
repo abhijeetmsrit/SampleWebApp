3ed29625b737beedebef1435b3e469e1c0f707cc
AK00420232 <AK00420232@INPUSCLP05989.TechMahindra.com>
2019-10-24 16:24:43 +0530

    First commit to git

---
+++ SampleWebApp/WebContent/Home.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="ISO-8859-1">
+    <title>Welcome user to the home page!</title>
+</head>
+<body>
+You have successfully logged in.
+</body>
+</html>
---
+++ SampleWebApp/src/com/abhi/model/User.java
@@ -0,0 +1,36 @@
+package com.abhi.model;
+
+public class User {
+	private String userid;
+	private String firstname;
+	private int mobileno;
+	private String emailid;
+	
+	
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public int getMobileno() {
+		return mobileno;
+	}
+	public void setMobileno(int mobileno) {
+		this.mobileno = mobileno;
+	}
+	public String getEmailid() {
+		return emailid;
+	}
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+	
+	
+}
