06507c6f711e8f4d42c728ee8b4512325c213164
AK00420232 <AK00420232@INPUSCLP05989.TechMahindra.com>
2019-10-24 15:09:54 +0530

    First commit consisting of login page , home page and POJO

---
+++ FirstSampleWebApp/src/com/abhi/sample/controller/LoginServlet.java
@@ -0,0 +1,5 @@
+package com.abhi.sample.controller;
+
+public class LoginServlet {
+
+}
---
+++ FirstSampleWebApp/src/com/abhi/sample/model/AbhiUser.java
@@ -0,0 +1,46 @@
+package com.abhi.sample.model;
+
+public class AbhiUser {
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmailid() {
+		return emailid;
+	}
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+	public int getMobileno() {
+		return mobileno;
+	}
+	public void setMobileno(int mobileno) {
+		this.mobileno = mobileno;
+	}
+	private String userid;
+	private String password;
+	private String firstname;
+	private String lastname;
+	private String emailid;
+	private int mobileno;
+}
